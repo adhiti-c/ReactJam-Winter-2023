10415cd9340875c003532a96794224a242f4b4fd
Adhiti Chandramohan <adhiti@outlook.com>
2023-12-27 11:09:52 -0800

    Added isometric camera view

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -5,8 +5,9 @@ import Player from './objects/player';
 export default function Game() {
 
     return (
-        <Canvas>
-            <ambientLight />
+        <Canvas camera = {{ position: [2, 3, 2] }}>
+            <ambientLight args={[0xff0000]} intensity={0.9} />
+            <directionalLight position={[0, 20, 10]} intensity={0.5} />
             <Player />
         </Canvas>
     )
