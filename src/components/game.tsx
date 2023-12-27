2895a820a88cc7a8b81ac19526b46164c9b674a2
Dylan Vu <dylanvu9@gmail.com>
2023-12-26 21:28:14 -0800

    add a box

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -1,11 +1,13 @@
 // the game world, including the cake, syrup, players, and items
 import { Canvas } from '@react-three/fiber';
+import Player from './objects/player';
 
 export default function Game() {
 
     return (
         <Canvas>
             <ambientLight />
+            <Player />
         </Canvas>
     )
 }
