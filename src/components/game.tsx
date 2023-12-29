a55fa941e6464e9cb06d74a98a502ab88997ec9d
Dylan Vu <dylanvu9@gmail.com>
2023-12-29 13:52:24 -0800

    make player object controllable/not controllable and move the variables to be a vector3

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -7,8 +7,8 @@ import Camera from './objects/camera'
 export default function Game() {
     const camera_pos = Camera();
     return (
-        <Canvas camera={{ position: [camera_pos[0],camera_pos[1],camera_pos[2]]}}>
-            <Player/>
+        <Canvas camera={{ position: [camera_pos[0], camera_pos[1], camera_pos[2]] }}>
+            <Player controllable={true} />
             <ambientLight args={[0xff0000]} intensity={0.5} />
             <directionalLight position={[0, 20, 10]} intensity={0.5} />
         </Canvas>
--- src/components/objects/player.tsx
+++ src/components/objects/player.tsx
@@ -1,10 +1,24 @@
 // contains player object and player logic
 import React, { useState, KeyboardEvent, useRef } from "react";
+import { Vector3 } from "three";
 
-export default function Player() {
-  const pos = PlayerMove();
+export default function Player({ controllable, position }: { controllable: boolean, position?: Vector3 }) {
+  // if the player is controllable, then player position should be controlled via arrow keys
+  let pos = new Vector3(0, 0, 0);
+  if (controllable) {
+    pos = PlayerMove();
+  } else {
+    // otherwise, we control it using the inputted state
+    // player is not controllable
+    if (!position) {
+      // a position should always be given
+      throw new Error("")
+    } else {
+      pos = position
+    }
+  }
   return (
-    <mesh position={[pos[0], pos[1], pos[2]]}>
+    <mesh position={new Vector3(pos.x, pos.y, pos.z)}>
       <boxGeometry />
       <meshStandardMaterial />
     </mesh>
@@ -12,51 +26,50 @@ export default function Player() {
 }
 
 //Player Movement
-function PlayerMove() {
-    const [position, setPosition] = useState([0, 0, 0]);
-    const moveSpeed = 0.1;
-    const windowSize = useRef([window.innerWidth, window.innerHeight]);
+function PlayerMove(): Vector3 {
+  const [position, setPosition] = useState([0, 0, 0]);
+  const moveSpeed = 0.1;
+  const windowSize = useRef([window.innerWidth, window.innerHeight]);
 
-    const handleMove = (e: KeyboardEvent):void => {
-      const newPosition = [...position];
-      if (e.key === "a") {
-        newPosition[0] -= moveSpeed;
-        newPosition[2] += moveSpeed;
-        console.log("left", e.key);
-        console.log(windowSize.current[0]);
-        console.log(position[0]);
-      } else if (e.key === "d") {
-        newPosition[0] += moveSpeed;
-        newPosition[2] -= moveSpeed;
-        console.log("right", position[0]);
-        console.log("right", e.key);
-      } else if (e.key === "w") {
-        newPosition[0] -= moveSpeed;
-        newPosition[2] -= moveSpeed;
-        console.log("up", e.key);
-      } else if (e.key === "s") {
-        newPosition[0] += moveSpeed;
-        newPosition[2] += moveSpeed;
-        console.log("down", e.key);
-      }
-      setPosition(newPosition);
+  const handleMove = (e: KeyboardEvent): void => {
+    const newPosition = [...position];
+    if (e.key === "a") {
+      newPosition[0] -= moveSpeed;
+      newPosition[2] += moveSpeed;
+      console.log("left", e.key);
+      console.log(windowSize.current[0]);
+      console.log(position[0]);
+    } else if (e.key === "d") {
+      newPosition[0] += moveSpeed;
+      newPosition[2] -= moveSpeed;
+      console.log("right", position[0]);
+      console.log("right", e.key);
+    } else if (e.key === "w") {
+      newPosition[0] -= moveSpeed;
+      newPosition[2] -= moveSpeed;
+      console.log("up", e.key);
+    } else if (e.key === "s") {
+      newPosition[0] += moveSpeed;
+      newPosition[2] += moveSpeed;
+      console.log("down", e.key);
+    }
+    setPosition(newPosition);
+  };
+
+  // Attach keydown event listener when the component mounts
+  React.useEffect(() => {
+    const keydownHandler = (event: Event): void => {
+      handleMove(event as unknown as KeyboardEvent<Element>);
     };
-  
-    // Attach keydown event listener when the component mounts
-    React.useEffect(() => {
-      const keydownHandler = (event: Event): void => {
-        handleMove(event as unknown as KeyboardEvent<Element>);
-      };
-      
-      document.addEventListener("keydown", keydownHandler);
-      // Clean up the event listener when the component unmounts
-      return () => {
-        document.removeEventListener("keydown", keydownHandler);
-      };
-    }, [position]);
-    
 
-    return (
-        position
-    )
+    document.addEventListener("keydown", keydownHandler);
+    // Clean up the event listener when the component unmounts
+    return () => {
+      document.removeEventListener("keydown", keydownHandler);
+    };
+  }, [position]);
+
+
+  return new Vector3(position[0], position[1], position[2])
+
 }
