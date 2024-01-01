bb0317012a192a69c2ac9e7a2c24ae39fa5a0769
Dylan Vu <dylanvu9@gmail.com>
2023-12-31 16:12:01 -0800

    format documentation correctly for game state

--- src/logic_v2/types.ts
+++ src/logic_v2/types.ts
@@ -15,20 +15,59 @@ export interface Player {
 }
 
 export interface GameState {
-    lastCountdown: number,                  // to track time at last countdown in milliseconds
-    timeLeft: number,                       // amount of time left in milliseconds
-    phase: GamePhase,                       // the current phase of the game
-    feedback: Feedback,                     // the feedback aka whether the build failed or was successful, or if it's in progress
-    players: Record<PlayerId, Player>,      // information about each player. Keyed by player id with value of the player object
-    cake: CakeLayerType[],                  // what has already been built, essentially, what has been built before the current goal
-    score: number,                          // score/number of layers
-    goal: GoalType,                         // the current goal
-    newLayer: CakeLayerType[],              // what has been built while trying to achieve the current goal
-    hint: {                                 // hint as to how to build the newest learned GoalType
-        recipe: Recipe,                         // recipe for the GoalType
-        count: number                           // number of times baked
+    /**
+     * to track time at last countdown in milliseconds
+     */
+    lastCountdown: number,
+    /**
+     * amount of time left in milliseconds
+     */
+    timeLeft: number,
+    /**
+     * the current phase of the game
+     */
+    phase: GamePhase,
+    /**
+     * the feedback aka whether the build failed or was successful, or if it's in progress
+     */
+    feedback: Feedback,
+    /**
+     * information about each player. Keyed by player id with value of the player object
+     */
+    players: Record<PlayerId, Player>,
+    /**
+     * what has already been built, essentially, what has been built before the current goal
+     */
+    cake: CakeLayerType[],
+    /**
+     * score/number of layers
+     */
+    score: number,
+    /**
+     * the current goal. What the players need to build next.
+     */
+    goal: GoalType,
+    /**
+     * what has been built while trying to achieve the current goal. May be wrong
+     */
+    newLayer: CakeLayerType[],
+    /**
+     * hint as to how to build the newest learned GoalType
+     */
+    hint: {
+        /**
+         * recipe for the GoalType
+         */
+        recipe: Recipe,
+        /**
+         * number of times this hint has been baked. Once it reaches a certain threshold, the players should have it memorized
+         */
+        count: number
     },
-    recipesLearned: Set<GoalType>           // set of goals encountered
+    /**
+     *  set of goals whose hints have already been completed. Aka, the goals whose recipes the player should have memorized by now.
+     */
+    recipesLearned: Set<GoalType>
 }
 
 type GameActions = {
