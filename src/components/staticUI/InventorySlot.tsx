a3df2c4fc4a55fedd80323205ebf81f80ed6c11c
Jasmine <jasmineqw45@gmail.com>
2024-01-01 00:06:01 -0800

    test

--- src/components/staticUI/InventorySlot.tsx
+++ src/components/staticUI/InventorySlot.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types';
 function InventorySlot({icon, onClick}) {
   const handleClick = () => {
-    // Call the onClick function passed as a prop that can be called in <Resourceitems />
     if (onClick) {
       onClick();
     }
