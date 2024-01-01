e4c3ab265f982c764d29152653cfbb28be97dff0
Dylan Vu <dylanvu9@gmail.com>
2023-12-31 17:09:52 -0800

    add documentation on configuration file and number of repeats for a hint to go away

--- src/logic_v2/logicConfig.ts
+++ src/logic_v2/logicConfig.ts
@@ -1,2 +1,14 @@
+/**
+ * amount of time starting out before players lose
+ */
 const StartTimeSeconds = 15;
+
+/**
+ * amount of time starting out before players lose, in milliseconds. Modify the StartTimeSeconds in `logicConfig.ts` instead of this value 
+ */
 export const StartTimeLeftMilliseconds = StartTimeSeconds * 1000;
+
+/**
+ * how many times the players must build the current hint before it leaves
+ */
+export const HintRepeatCount = 3;
