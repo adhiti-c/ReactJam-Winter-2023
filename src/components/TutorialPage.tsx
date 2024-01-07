c0e2b3acb8933b6784120cb0954966bde4b6a674
Jasmine <jasmineqw45@gmail.com>
2024-01-06 16:48:56 -0800

    compeltely fleshed out tutorial pages and fixed some viewport issues for mobiled

--- src/components/TutorialPage.tsx
+++ src/components/TutorialPage.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
-function TutorialPage() {
+function TutorialPage({currentPage, title, img}:{currentPage: number, title: React.ReactNode, img: string}) {
   return (
     <div className='tutorial-page-contain'>
-      
+      <div className="top-contain">
+        <div className="number-contain">
+            {currentPage}
+            
+        </div>
+        
+            {title}
+        
+      </div>
+      <img src={img} alt="" />
     </div>
   )
 }
