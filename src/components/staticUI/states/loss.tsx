68eb63d615e223cbcb69ae550c16841e333458d8
Dylan Vu <dylanvu9@gmail.com>
2024-01-07 23:56:31 -0800

    add loss sound

--- src/components/staticUI/states/loss.tsx
+++ src/components/staticUI/states/loss.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { GameState } from '../../../logic_v2/types';
+import LossSound from "../../../assets/lossSound.wav";
 //* component for high score screen
 function loss({ game }: { game: GameState }) {
+
+  useEffect(() => {
+    playSound("lossSound")
+  }, [])
+
+  function playSound(soundId: string) {
+    const sound = document.getElementById(soundId) as HTMLAudioElement;
+    if (sound) {
+      sound.play();
+    }
+  }
+
   return (
     <div className='high-score-contain'>
       <div className="high-score-item">
@@ -20,6 +33,9 @@ function loss({ game }: { game: GameState }) {
         <FontAwesomeIcon icon={faReplay}/>
             Replay
       </button> */}
+      <audio id="lossSound" preload="auto">
+        <source src={LossSound} type="audio/wav" />
+      </audio>
     </div>
   )
 }
